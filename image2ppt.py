45a9d2db39a41505917e6f0942f3817d49d80f9f
Enes Çakır <enes@cakir.web.tr>
2017-08-02 01:08:40 +0300

    Add GUI

--- image2ppt.py
+++ image2ppt.py
@@ -2,50 +2,130 @@
 import sys, os
 from pptx import Presentation
 from pptx.util import Inches
+import Tkinter
+from Tkinter import *
+import ScrolledText as tkst
+import tkFileDialog as filedialog
 
-INPUT_PATH = os.getcwd()
-OUTPUT_PATH = os.getcwd()
-PPT_NAME = "image2ppt"
-SLIDE_WIDTH = 1920
-SLIDE_HEIGHT = 1080
-RESOLUTION = 72
-PPT_NAME = "image2ppt"
+DEFAULT_INPUT_PATH = os.path.expanduser("~/Desktop/")
+DEFAULT_OUTPUT_PATH = os.path.expanduser("~/Desktop/")
+DEFAULT_PPT_NAME = "image2ppt"
+DEFAULT_SLIDE_WIDTH = 1920
+DEFAULT_SLIDE_HEIGHT = 1080
+DEFAULT_RESOLUTION = 72
 SUPPORTED_FORMATS = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG' ]
 
-def pixelToEmu(pixel, resolution):
-    return int(int(pixel) * 914400 / int(resolution))
+ppt_name = DEFAULT_PPT_NAME
+input_path = DEFAULT_INPUT_PATH
+output_path = DEFAULT_OUTPUT_PATH
+slide_width = DEFAULT_SLIDE_WIDTH
+slide_height = DEFAULT_SLIDE_HEIGHT
+resolution = DEFAULT_RESOLUTION
 
-img_folder = raw_input("Enter path of image folder [Current directory]: ").strip() or INPUT_PATH
-output_name = raw_input("Enter name of presentation [" + PPT_NAME + "]: ").strip() or PPT_NAME
-output_path = raw_input("Enter output path [Current directory]: ").strip() or OUTPUT_PATH
-width_pixel = raw_input("Image width in pixels [" + str(SLIDE_WIDTH) + "]: ") or SLIDE_WIDTH
-height_pixel = raw_input("Image height in pixels [" + str(SLIDE_HEIGHT) + "]: ") or SLIDE_HEIGHT
-img_resolution = raw_input("Image resolution in ppi [" + str(RESOLUTION) + "]:") or RESOLUTION
+def pixelToEmu(pix):
+    return int(int(pix) * 914400 / int(resolution))
 
-img_width = pixelToEmu(width_pixel, img_resolution)
-img_height = pixelToEmu(height_pixel, img_resolution)
+def getInputFolder():
+    global input_path
+    input_path = filedialog.askdirectory()
+    l5.config(text=input_path)
 
-files = os.listdir(img_folder)
-images = []
-for f in files:
-    path = os.path.join(img_folder, f)
-    if os.path.isfile(path):
-        filename, extension = os.path.splitext(f)
-        if extension in SUPPORTED_FORMATS:
-            images.append(path)
+def getOutputFolder():
+    global output_path
+    output_path = filedialog.askdirectory()
+    l6.config(text=output_path)
 
-print(str(len(images)) + " images found")
+def generate():
+    global ppt_name, resolution, slide_width, slide_height
+    ppt_name = e1.get()
+    resolution = int(e4.get())
+    slide_width = pixelToEmu(e2.get())
+    slide_height = pixelToEmu(e3.get())
 
-prs = Presentation()
-prs.slide_height = img_height
-prs.slide_width = img_width
+    files = os.listdir(input_path)
+    images = []
+    for f in files:
+        path = os.path.join(input_path, f)
+        if os.path.isfile(path):
+            filename, extension = os.path.splitext(f)
+            if extension in SUPPORTED_FORMATS:
+                images.append(path)
 
-for image in images:
-    blank_slide_layout = prs.slide_layouts[6]
-    slide = prs.slides.add_slide(blank_slide_layout)
-    pic = slide.shapes.add_picture(image, Inches(0) , Inches(0), img_width,  img_height)
-    print( "\"" + os.path.basename(image) + "\" is added")
+    print(str(len(images)) + " images found")
+
+    prs = Presentation()
+    prs.slide_width = slide_width
+    prs.slide_height = slide_height
+
+    for image in images:
+        blank_slide_layout = prs.slide_layouts[6]
+        slide = prs.slides.add_slide(blank_slide_layout)
+        pic = slide.shapes.add_picture(image, Inches(0) , Inches(0), slide_width,  slide_height)
+        print( "\"" + os.path.basename(image) + "\" is added")
 
 
-prs.save(os.path.join(output_path, output_name + '.pptx'))
-print(output_name + '.pptx is saved')
+    prs.save(os.path.join(output_path, ppt_name + '.pptx'))
+    print(ppt_name + '.pptx is saved')
+
+
+root = Tk()
+root.resizable(width=False, height=False)
+root.minsize(width=500, height=420)
+root.wm_title("image2ppt")
+
+l0 = Label(root, text="image2ppt", font = "Helvetica 24 bold")
+l1 = Label(root, text="Presentation's name:", anchor=W)
+l2 = Label(root, text="Image width in pixels:", anchor=W)
+l3 = Label(root, text="Image height in pixels:", anchor=W)
+l4 = Label(root, text="Image resolution in ppi:", anchor=W)
+
+l0.place(x = 150, y = 10, width=200, height=40)
+l1.place(x = 100, y = 60, width=160, height=30)
+l2.place(x = 100, y = 100, width=160, height=30)
+l3.place(x = 100, y = 140, width=160, height=30)
+l4.place(x = 100, y = 180, width=160, height=30)
+
+e1 = Entry(root)
+e2 = Entry(root)
+e3 = Entry(root)
+e4 = Entry(root)
+
+e1.insert(0, DEFAULT_PPT_NAME)
+e2.insert(0, DEFAULT_SLIDE_WIDTH)
+e3.insert(0, DEFAULT_SLIDE_HEIGHT)
+e4.insert(0, DEFAULT_RESOLUTION)
+
+e1.place(x = 280, y = 60, width=120, height=30)
+e2.place(x = 280, y = 100, width=120, height=30)
+e3.place(x = 280, y = 140, width=120, height=30)
+e4.place(x = 280, y = 180, width=120, height=30)
+
+l5 = Label(root, text=DEFAULT_INPUT_PATH, anchor=W)
+b1 = Button(root, text='Choose Images', command=getInputFolder)
+l5.place(x = 20, y = 220, width=330, height=30)
+b1.place(x = 360, y = 220, width=120, height=30)
+
+l6 = Label(root, text=DEFAULT_OUTPUT_PATH, anchor=W)
+b2 = Button(root, text='Choose Output', command=getOutputFolder)
+l6.place(x = 20, y = 260, width=330, height=30)
+b2.place(x = 360, y = 260, width=120, height=30)
+
+b3 = Button(root, text='Generate Presentation', command=generate)
+b3.place(x = 100, y = 300, width=300)
+
+tb = tkst.ScrolledText(
+    master = root,
+    wrap   = 'word',  # wrap text at full words only
+    width  = 25,      # characters
+    height = 10,      # text lines
+    # state=DISABLED,
+    borderwidth=2,
+    relief="groove"
+)
+tb.place(x = 50, y = 340, width=400, height=60)
+tb.insert('insert', "DENEME 1\n")
+tb.insert('insert', "DENEME 2\n")
+tb.insert('insert', "DENEME 3\n")
+tb.insert('insert', "DENEME 4\n")
+
+root.mainloop()
